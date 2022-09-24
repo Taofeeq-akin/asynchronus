861592c55b8305f9a8d11aaef69b1495af504f20
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-09-24 07:51:22 +0100

    Now i get details about Nigeria

--- script.js
+++ script.js
@@ -5,4 +5,16 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 const request = new XMLHttpRequest();
+// Bsically open request
 request.open('GET', 'https://restcountries.com/v3.1/name/nigeria');
+// then we eed to send, willl keep running in asynchronous way
+// data = request.send(); // cant do this cause request is not avalible
+request.send();
+
+request.addEventListener('load', function () {
+  //  console.log(this.responseText) // but we got a big string instead
+  // To get an object we use
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+});
+
