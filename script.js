ce8aa13f1ddddba6f43f52708fbd828dc42cd807
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-09-24 14:27:49 +0100

    Get neighbouring country

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 // TO get multiple countries
 
-const getCountryData = function (country) {
+/*const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   // Bsically open request
   request.open('GET', `https://restcountries.com/v2/name/${country}`);
@@ -44,4 +44,59 @@ const getCountryData = function (country) {
 // they may not arrive accordingly on the interface cus they load faster then each other, This makes it  visible of a AJAx asynchronous JS
 getCountryData('nigeria')
 getCountryData('usa')
-getCountryData('united kingdom')
+getCountryData('united kingdom')*/
+
+// Loading datas accordingly with there neighbouring country
+const renderCountry = function (data, classname = '') {
+  const html = `
+  <article class="country ${classname}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}
+    </div>
+  </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  //set country opacity to one
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+  // Bsically open request
+  // AJAX call country 1
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // render country
+    renderCountry(data);
+
+    // Get neighbour country
+    const [neighbour] = data.borders;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+getCountryAndNeighbour('nigeria');
+getCountryAndNeighbour('united kingdom');
