8ffca86f624fe9e21190e92e9fdbb83d8067cc85
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-09-24 07:36:35 +0100

    using XMLHttpRequest function

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/name/nigeria');
