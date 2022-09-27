3ba4cca016c7c8255a1c4b7e22114602d9c5e8d1
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-09-27 16:30:45 +0100

    Used promise.all method to run all promises at once

--- script.js
+++ script.js
@@ -479,11 +479,21 @@ console.log('1: Wil get loction');
 // Runing Promises in Parellel
 const get3Countries = async function (c1, c2, c3) {
   try {
-    const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
-    const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
-    const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+    // This will run each function one after the other
+    // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
 
-    console.log([data1.capital, data2.capital, data3.capital]);
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    // But if need to run all at once without waiting for each other, we can go like this
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v2/name/${c1}`),
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ]);
+
+    console.log(data.map(eachArray => eachArray[0].capital));
   } catch (err) {
     console.error(err.message);
   }
